327cf55c7dc08c0cd808134ac07ca8e44c9068b3
Morgován Nimród <morgovannimrod22@gmail.com>
2022-10-06 08:39:31 +0200

    Véletlen osztály - statikus viselkedés:   - az osztály final   - privát konstruktor - Random adattag - Listák nevekkeé - Véletlen szám generálása

--- src/hu/petrik/statikusosztalyok/Main.java
+++ src/hu/petrik/statikusosztalyok/Main.java
@@ -1,7 +1,18 @@
 package hu.petrik.statikusosztalyok;
 
 public class Main {
-    public static void main(String[] args) {
+  //  int nemStatikusAdattag;
+  //  static int statikusTag;
 
+    public static void main(String[] args) {
+       /*
+        Main obj = new Main();
+        System.out.println(obj.nemStatikusAdattag);
+        System.out.println(Main.statikusTag);
+       */
+        for (int i = 0; i < 100; i++) {
+            System.out.println(Veletlen.velEgesz(5,10));
+        }
     }
+
 }
